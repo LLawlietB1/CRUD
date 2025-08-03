eaa4e47a96c0c40bd6737e4e3274c955a6e5ab35
Lucas Nogueira <167033268+LLawlietB1@users.noreply.github.com>
2025-08-03 16:12:50 -0300

    never use double slashes
    While studying, I learned that when using $, we cannot use double slashes.

--- crud.Infrastructure/crudDbContextExample.cs
+++ crud.Infrastructure/crudDbContextExample.cs
@@ -9,7 +9,7 @@ namespace crud.Infrastructure
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\caminho\\para\\seu\\banco.mdf;Integrated Security=True;");
+            optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\caminho\para\seu\banco.mdf;Integrated Security=True;");
 
         }
     }
